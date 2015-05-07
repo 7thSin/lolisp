dfb1fbd558df8e62d132028084d18d1fb2fdb786
sovietspaceship <enma.sari@gmail.com>
2015-05-08 01:35:38 +0200

    Removed debug code

--- src/eval.cpp
+++ src/eval.cpp
@@ -82,7 +82,6 @@ inline obj* call_lambda(obj* ptr, obj* args) {
     }
     advance(ptr);
     for iterate_elements(ptr, it) {
-        cout << exprtrace(it) << endl;
         ret = eval(it);
     }
     for (auto const& it : local)
